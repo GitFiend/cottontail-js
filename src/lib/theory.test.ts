300b7d305a4a139b363d69e99109ba23be157609
Toby <sugto555@gmail.com>
2023-10-20 10:39:05 +1300

    Add a benchmark to test the performance theory behind library.

---
+++ src/lib/theory.test.ts
@@ -0,0 +1,99 @@
+describe('benchmark to get a sense of how fast it will be to compare lots of objects per frame', () => {
+  test('lots of object creations and comparisons', () => {
+    const numComparisons = 1000
+    const objects1 = []
+    const objects2 = []
+
+    for (let i = 0; i < numComparisons; i++) {
+      const o1 = makeTestObject()
+      objects1.push(o1)
+
+      if (i % 2 === 0) {
+        objects2.push({...o1})
+      } else {
+        objects2.push(makeTestObject())
+      }
+    }
+
+    const start = Date.now()
+
+    const equal: boolean[] = []
+
+    for (let i = 0; i < objects1.length; i++) {
+      const o1 = objects1[i]
+      const o2 = objects2[i]
+
+      // We do object spread to simulate selecting state and putting it into new object
+      equal.push(equalValues(o1, {...o2}))
+    }
+
+    const time = Date.now() - start
+
+    console.log(`${time}ms to compare ${numComparisons} prop objects`)
+
+    expect(equal.length).toBe(objects1.length)
+    expect(time).toBeLessThan(16)
+  })
+})
+
+function makeTestObject(): Record<string, unknown> {
+  const keys = [
+    'a',
+    'b',
+    'cccc',
+    'dddd',
+    'eeeeeeeeeeeeeeeee',
+    'f',
+    'ggg',
+    'hh',
+    'iiiiiiii',
+    'jj',
+  ]
+  shuffleArray(keys)
+
+  const values = [
+    3,
+    'text',
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    {a: 4, b: 3, c: keys},
+    'asdf asdfas dfasdf',
+    {x: 1, y: 1},
+    false,
+    true,
+    {a: {a: {a: {a: {a: {}, b: 3, c: {a: 2}}}}}},
+    1234567,
+  ]
+
+  const testObject: Record<string, unknown> = {}
+
+  while (keys.length > 0) {
+    const key = keys.pop()!
+    testObject[key] = values.pop()!
+  }
+
+  return testObject
+}
+
+// Assumes keys are the same in both objects
+function equalValues(a: Record<string, unknown>, b: Record<string, unknown>): boolean {
+  for (const key of Object.keys(a)) {
+    if (a[key] !== b[key]) return false
+  }
+
+  return true
+}
+
+function shuffleArray(array: unknown[]): void {
+  let currentIndex = array.length,
+    randomIndex
+
+  // While there remain elements to shuffle.
+  while (currentIndex > 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex--
+
+    // And swap it with the current element.
+    ;[array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]]
+  }
+}
