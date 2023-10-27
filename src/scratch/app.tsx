77e87ce032080b0b3522ec5cc84f4b370750a86e
Toby <sugto555@gmail.com>
2023-10-27 20:45:37 +1300

    Misc.

--- src/scratch/app.tsx
+++ src/scratch/app.tsx
@@ -2,14 +2,18 @@ import {render} from '../lib/render'
 import {createElement} from '../create-element'
 
 render(
-  <div
-    style={{
-      width: `100px`,
-      height: `100px`,
-      background: 'red',
-    }}
-  >
-    <span style={{fontSize: '100px'}}>Hello</span>
+  <div>
+    <div
+      style={{
+        width: `100px`,
+        height: `100px`,
+        background: 'red',
+        zIndex: 0,
+      }}
+    >
+      <span style={{fontSize: '100px'}}>Hello</span>
+      <span>hi</span>
+    </div>
   </div>,
   document.getElementById('root'),
 )
