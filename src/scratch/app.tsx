ae3283298b8009fe8b5ee9a871edf63592c34057
Toby <sugto555@gmail.com>
2023-10-29 12:03:41 +1300

    Expand example to test lots of components updating when shared state changes.

--- src/scratch/app.tsx
+++ src/scratch/app.tsx
@@ -2,9 +2,26 @@ import {createElement} from '../create-element'
 import {renderRoot} from '../lib/cottontail'
 import {Component} from '../lib/components/custom-component'
 
-class Thing extends Component<{}> {
+class Store {
+  num = 0
+
+  get square() {
+    return this.num ** 2
+  }
+
+  onClick = () => {
+    this.num++
+    render()
+  }
+}
+
+class Thing extends Component<{store: Store}> {
   state = {
     num: 0,
+  }
+
+  selectState(props: {store: Store}) {
+    this.state.num = props.store.num
   }
 
   render() {
@@ -17,22 +34,60 @@ class Thing extends Component<{}> {
         }}
       >
         <span style={{fontSize: '100px'}}>Hello</span>
-        <button style={{width: '120px'}} onClick={this.onClick}>
+        <button style={{width: '120px'}} onClick={this.props.store.onClick}>
           {`Num Clicks: ${this.state.num}`}
         </button>
       </div>
     )
   }
-
-  onClick = () => {
-    this.state.num++
-    render()
-  }
 }
 
+const store = new Store()
+
 export const {render} = renderRoot(
-  <div>
-    <Thing />
+  <div style={{display: 'flex', flexDirection: 'column'}}>
+    <div style={{height: '150px'}}>
+      <Thing store={store} />
+    </div>
+    <div style={{height: '150px'}}>
+      <Thing store={store} />
+    </div>
+    <div style={{height: '150px'}}>
+      <Thing store={store} />
+    </div>
+    <div style={{height: '150px'}}>
+      <Thing store={store} />
+    </div>
+    <div style={{height: '150px'}}>
+      <Thing store={store} />
+    </div>
+    <div style={{height: '150px'}}>
+      <Thing store={store} />
+    </div>
+    <div style={{height: '150px'}}>
+      <Thing store={store} />
+    </div>
+    <div style={{height: '150px'}}>
+      <Thing store={store} />
+    </div>
+    <div style={{height: '150px'}}>
+      <Thing store={store} />
+    </div>
+    <div style={{height: '150px'}}>
+      <Thing store={store} />
+    </div>
+    <div style={{height: '150px'}}>
+      <Thing store={store} />
+    </div>
+    <div style={{height: '150px'}}>
+      <Thing store={store} />
+    </div>
+    <div style={{height: '150px'}}>
+      <Thing store={store} />
+    </div>
+    <div style={{height: '150px'}}>
+      <Thing store={store} />
+    </div>
   </div>,
   document.getElementById('root'),
 )
